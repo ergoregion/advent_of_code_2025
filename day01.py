78aa549ad1670db0ea38d81920de31f5c2ea2a47
Neil Butcher <n.a.butcher@bham.ac.uk>
2025-11-28 15:49:15 +0000

    getting some things ready

---
+++ day01.py
@@ -0,0 +1,11 @@
+with open('data/day01_test.txt', 'r') as file:
+    test_split_lines = [i.split() for i in file.readlines()]
+
+with open('data/day01.txt', 'r') as file:
+    real_split_lines = [i.split() for i in file.readlines()]
+
+print(test_split_lines)
+print(real_split_lines)
+
+split_lines = test_split_lines
+# split_lines = real_split_lines
