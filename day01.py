bfbd41d7859817fa4e39783742e0be8cf8fca62b
Neil Butcher <n.a.butcher@bham.ac.uk>
2025-12-01 09:40:45 +0000

    day 1

--- day01.py
+++ day01.py
@@ -1,11 +1,38 @@
 with open('data/day01_test.txt', 'r') as file:
-    test_split_lines = [i.split() for i in file.readlines()]
+    test_lines = file.readlines()
 
 with open('data/day01.txt', 'r') as file:
-    real_split_lines = [i.split() for i in file.readlines()]
+    real_lines = file.readlines()
 
-print(test_split_lines)
-print(real_split_lines)
+# print(test_split_lines)
+# print(real_split_lines)
 
-split_lines = test_split_lines
-# split_lines = real_split_lines
+lines = real_lines
+
+
+print(test_lines)
+
+count = 0
+dial = 50
+for n in lines:
+    sign = -1 if n[0] == 'L' else 1
+    value = int(str(n[1:]))
+
+    dial = (dial + (sign * value)) % 100
+    if (dial == 0):
+        count = count+1
+
+print(count)
+
+count = 0
+dial = 50
+for n in lines:
+    sign = -1 if n[0] == 'L' else 1
+    value = int(str(n[1:]))
+    for _ in range(value):
+        dial = (dial + (sign * 1)) % 100
+        if (dial == 0):
+            count = count+1
+    print(f"count at {n} : {count}")
+
+print(count)
