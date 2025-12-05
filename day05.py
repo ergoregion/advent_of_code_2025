58029c5eca549d8606397d1ddde8817305441b14
Neil Butcher <n.a.butcher@bham.ac.uk>
2025-12-05 10:21:48 +0000

    neater solution

--- day05.py
+++ day05.py
@@ -1,74 +1,53 @@
 with open('data/day05_test.txt', 'r') as file:
-    test_lines = file.readlines()
+    test_data = file.read()
 
 with open('data/day05.txt', 'r') as file:
-    real_lines = file.readlines()
-
-with open('data/day05_a_test.txt', 'r') as file:
-    test_ing = file.readlines()
-
-with open('data/day05_a.txt', 'r') as file:
-    real_ing = file.readlines()
+    real_data = file.read()
 
 
-lines = test_lines
-lines = real_lines
-ing = test_ing
-ing = real_ing
+data = test_data
+data = real_data
 
-print(lines)
-print(ing)
+sections = data.split("\n\n")
 
-ranges = [[int(i) for i in l.strip().split('-')]for l in lines]
-print(ranges)
+lines = sections[0].split("\n")
+ingredients = sections[1].split("\n")
+
+# print(lines)
+# print(ingredients)
+
+inclusive_ranges = [[int(i) for i in l.strip().split('-')]for l in lines]
+ranges = [[r[0], r[1]+1] for r in inclusive_ranges]
+# print(ranges)
+
 
 def is_valid(ing):
     for r in ranges:
-        if(r[0]<=ing<=r[1]):
+        if (r[0] <= ing < r[1]):
             return True
     return False
 
+
 result = 0
-valid = [i for i in ing if is_valid(int(i))]
-print(valid)
+valid = [i for i in ingredients if is_valid(int(i))]
+# print(valid)
 print(f"result part 1: {len(valid)}")
 
-ranges = [(r[0],r[1]) for r in ranges]
-
-def ranges_overlap(r1, r2):
-    overlap = max(0, min(r1[1]+1, r2[1]+1) - max(r1[0], r2[0]))
-    return overlap>0
-
-def consolidate_ranges(r1, r2):
-    return min(r1[0], r2[0]), max(r1[1], r2[1])
-
-def add_range(r,existing_ranges):
-    for re in existing_ranges:
-        if ranges_overlap(r,re):
-            existing_ranges.remove(re)
-            existing_ranges.add(consolidate_ranges(r, re))
-            return
-    existing_ranges.add(r)
-
-consolidated_ranges = set()
-for r in ranges:
-    add_range(r, consolidated_ranges)
-print(len(consolidated_ranges))
-old_count = len(ranges)
-new_count = len(consolidated_ranges)
-while new_count<old_count:
-    old_count = len(consolidated_ranges)
-    old_ranges = consolidated_ranges
-    consolidated_ranges = set()
-    for r in old_ranges:
-        add_range(r, consolidated_ranges)
-    print(len(consolidated_ranges))
-    new_count = len(consolidated_ranges)
-
+# sorting by start condition, means we can look for overlaps in adjacent ranges
+ranges.sort(key=lambda r: r[0])
 
 
 result = 0
-for r in consolidated_ranges:
-    result += r[1]+1-r[0]
+active_range = ranges[0]
+for r in ranges[1:]:
+    if r[0] <= active_range[1]:
+        # make the current active range bigger
+        active_range[1] = max(r[1], active_range[1])
+    else:
+        # add the current active range to the result & start a new one
+        result += active_range[1]-active_range[0]
+        active_range = r
+# add the final range
+result += active_range[1]-active_range[0]
+
 print(f"result part 2: {result}")
-    
